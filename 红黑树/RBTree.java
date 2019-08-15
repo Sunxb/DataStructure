924fe53d8c8f66fd2a498c35c877103f6d502c37
xiaobin.sun <xiaobin.sun@fumubang.com>
2019-08-15 14:01:35 +0800

    红黑树

--- 红黑树/RBTree.java
+++ 红黑树/RBTree.java
@@ -76,16 +76,94 @@ public class RBTree<T> extends BBST<T> {
 	
 	@Override
 	protected void afterRemove(Node<T> node, Node<T> replace) {
-		// 红色结点删除完不需要处理
-		if (isRed(node)) {
+		// 1. 红色结点删除完不需要处理
+		if (isRed(node)) return;
+//		
+//		// 我们这里的replace其实就是其中一个子结点
+//		// 如果有node两个红色子结点的话, 那肯定删除的不是node, 所以被删除的node少于两个子结点(1或者0)
+//		// 2. 删除拥有一个红色子节点的node
+		if (isRed(replace)) {
+			black(replace);
 			return;
 		}
 		
-		// 我们这里的replace其实就是其中一个子结点
-		// 如果有node两个红色子结点的话, 那肯定删除的不是node, 所以被删除的node少于两个子结点(1或者0)
-		// 拥有一个红色子节点的node
-		if (isRed(replace)) {
+		Node<T> parent = node.parent;
+		// 根结点
+		if (parent == null) return;
+		
+		
+		// 走到这就是删除 黑叶子
+		
+		// 该结点在父结点的左还是右
+		boolean left = parent.left == null || node.isLeftChild();
+		// 兄弟结点
+		Node<T> sibling = left ? parent.right : parent.left;
+		
+		// 被删除的结点在左边 (与下面的在右边其实是操作对称的)
+		if (left) {
+			if (isRed(sibling)) {
+				black(sibling);
+				red(parent);
+				rotateLeft(parent);
+				sibling = parent.right;
+			}
+			if (isBlack(sibling.left) && isBlack(sibling.right)) {
+				boolean parentBlack = isBlack(parent); // 提前判断 因为下面有把parent染黑的操作
+				black(parent);
+				red(sibling);
+				if (parentBlack) {
+					afterRemove(parent, null);
+				}
+			}
+			else {
+				if (isBlack(sibling.right)) {
+					rotateRight(sibling);
+					sibling = parent.right;
+				}
+				rotateLeft(parent);
+				color(sibling, colorOf(parent));
+				black(sibling.right);
+				black(parent);
+			}
+					
+		}
+		
+		// 被删除的结点在右边
+		else {
+			// 红兄弟的情况  经过转化可变为黑兄弟的情况 , 所以先处理红兄弟的情况
+			if (isRed(sibling)) {
+				black(sibling);
+				red(parent);
+				rotateRight(parent);
+				sibling = parent.left;
+			}
 			
+			// 黑兄弟
+			// 兄弟结点的两个子结点都是黑色 (两个结点都是null) 
+			if (isBlack(sibling.left) && isBlack(sibling.right)) {
+				boolean parentBlack = isBlack(parent); // 提前判断 因为下面有把parent染黑的操作
+				black(parent);
+				red(sibling);
+				if (parentBlack) {
+					afterRemove(parent, null);
+				}
+			}
+			// 兄弟结点至少有一个红色的子结点
+			else {
+				// 有三种情况  1仅有一个左  2一仅有个右  3左右都有
+				// 没有左的情况是LR 旋转两次 所以先处理这个情况 先左转 然后统一处理右转
+				if (isBlack(sibling.left)) {
+					//
+					rotateLeft(sibling);
+					sibling = parent.left;
+				}
+				
+				// 统一处理右转
+				rotateRight(parent);
+				color(sibling, colorOf(parent));
+				black(sibling.left);
+				black(parent);
+			}
 		}
 		
 	}
